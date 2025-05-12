cd60ce8ffe2653610545f956a40eb97e89345df3
Saniaparveen0196 <saania0196@gmail.com>
2025-05-12 13:36:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const Base_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropdowns =document.querySelectorAll('.select-container select');
+let btn = document.querySelector('form button');
+const fromCurr = document.querySelector('.from select');
+const toCurr = document.querySelector('.to select');
+const msg = document.querySelector('.msg');
+
+
+for( let select of dropdowns){
+    for(currCode in countryList){
+        let newOption= document.createElement('option');
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if (select.name ==='from' && currCode === 'USD'){
+            newOption.selected='selected';
+        } else if (select.name ==='to' && currCode ==='INR'){
+            newOption.selected = 'selected';
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener('change',(evt) =>{
+        updateFlag(evt.target);
+    });
+}
+const updateExchangeRate = async () =>{
+    let amountInput = document.querySelector('.amount input');
+    let amtVal = amountInput.value;
+
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amountInput.value = '1';
+    }
+
+    const URL = `${Base_URL}/${fromCurr.value.toLowerCase()}.json`;
+
+    try {
+        let response = await fetch(URL);
+        let data = await response.json();
+        let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+        let finalAmount = (amtVal * rate).toFixed(2);
+
+        msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    } catch (error) {
+        msg.innerText = "Something went wrong. Please try again.";
+        console.error(error);
+    }
+
+}
+const updateFlag = (element) =>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector('img');
+    img.src=newSrc;
+}
+btn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+    
+});
+window.addEventListener('load',() =>{
+    updateExchangeRate();
+
+});
+
+
